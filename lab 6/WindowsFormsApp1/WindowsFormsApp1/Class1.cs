4c60aa52010b6b6af401acfdcdd31576c0c61535
ZXL0ver <130565880+ZXL0ver@users.noreply.github.com>
2023-04-12 21:17:11 +0300

    Add files via upload

---
+++ lab 6/WindowsFormsApp1/WindowsFormsApp1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp1
+{
+    // Interfaces for XML data and JSON data
+    interface IXmlData
+    {
+        string GetXml();
+    }
+
+    interface IJsonData
+    {
+        string GetJson();
+    }
+
+    // XML to JSON adapter
+    class XmlToJsonAdapter : IJsonData
+    {
+        private readonly IXmlData _xmlData;
+
+        public XmlToJsonAdapter(IXmlData xmlData)
+        {
+            _xmlData = xmlData;
+        }
+
+        public string GetJson()
+        {
+            // Convert XML to JSON
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(_xmlData.GetXml());
+            return JsonConvert.SerializeXmlNode(xmlDoc);
+        }
+    }
+
+    // JSON to XML adapter
+    class JsonToXmlAdapter : IXmlData
+    {
+        private readonly IJsonData _jsonData;
+
+        public JsonToXmlAdapter(IJsonData jsonData)
+        {
+            _jsonData = jsonData;
+        }
+
+        public string GetXml()
+        {
+            // Convert JSON to XML
+            XmlDocument xmlDoc = JsonConvert.DeserializeXmlNode(_jsonData.GetJson());
+            return xmlDoc.InnerXml;
+        }
+    }
+}
---
+++ lab 6/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private IXmlData _xmlData;
+        private IJsonData _jsonData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _xmlData = new XmlData(textBox1.Text);
+            _jsonData = new XmlToJsonAdapter(_xmlData);
+            textBox2.Text = _jsonData.GetJson();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _jsonData = new JsonData(textBox1.Text);
+            _xmlData = new JsonToXmlAdapter(_jsonData);
+            textBox2.Text = _xmlData.GetXml();
+        }
+        class XmlData : IXmlData
+        {
+            private readonly string _xml;
+
+            public XmlData(string xml)
+            {
+                _xml = xml;
+            }
+
+            public string GetXml()
+            {
+                return _xml;
+            }
+        }
+
+        // JSON data class
+        class JsonData : IJsonData
+        {
+            private readonly string _json;
+
+            public JsonData(string json)
+            {
+                _json = json;
+            }
+
+            public string GetJson()
+            {
+                return _json;
+            }
+        }
+    }
+}
